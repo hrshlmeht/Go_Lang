c5c88b96716efe31e6f83bc1d208b6ef1f1de2e9
hrshlmeht <harshalmehta1998@gmail.com>
2023-05-03 02:09:25 -0400

    understanding the variables

---
+++ tutorial.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main(){
+
+	fmt.Println("Quiz game in go lang")
+
+	var name string = "Harshal"
+	fmt.Println(name)
+	fmt.Printf("Hello %v %v , how are you doing today" , name , name )
+
+}
+
