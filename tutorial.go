07014030edf829c59ed4581d051b45051a53b426
hrshlmeht <harshalmehta1998@gmail.com>
2023-05-03 20:49:43 -0400

    Understood the basics of go_lang

--- tutorial.go
+++ tutorial.go
@@ -23,14 +23,34 @@ func main(){
 	fmt.Printf("What is better , the RTX 3090 or GTX 3080? ")
 	var answer string
 	var answer2 string 
+	var  score int 
+	var answer3 string 
+	score = 0
 	fmt.Scan(&answer , &answer2)
 	fmt.Println(answer , answer2)
 
 	if answer + " " + answer2 == "RTX 3090"{
 		fmt.Println("Correct")
-	}else {
+		score++
+	} else if answer + " " + answer2 == "rtx 3090"{
+		fmt.Println("Correct")
+		score++
+	} else {
 		fmt.Println("Incorrect")
 	}
-}
+
+	fmt.Printf("Which university is better , Cornell or Gtech?")
+	fmt.Scan(&answer3)
+
+	if answer3 == "Cornell"{
+		fmt.Printf("Thats correct")
+		score++
+	} else {
+		fmt.Printf("Oops you have answered the question wrong")
+	}
+
+	fmt.Printf("The total score of  %v  is %v \n", name, score)
+
+}//main ends
 
 
